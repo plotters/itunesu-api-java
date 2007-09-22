5d7b57b1c52bb7ff1a5379cb5e2833825ece71c8
dave.benjamin <dave.benjamin@7fe4d8d9-7835-0410-8305-53dfd29555a2>
2007-09-22 05:04:54 +0000

    Merged file upload and JavaMail-free changes from trunk

--- branches/xsd-validation/src/edu/asu/itunesu/ITunesUFilePOST.java
+++ branches/xsd-validation/src/edu/asu/itunesu/ITunesUFilePOST.java
@@ -28,6 +28,10 @@
 package edu.asu.itunesu;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -39,15 +43,55 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMultipart;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 class ITunesUFilePOST {
     public String invokeAction(String url,
                                String name,
                                String fileName,
                                String data,
-                               String contentType) throws MessagingException {
+                               String contentType) {
+        InputStream dataStream;
+        int contentLength = 0;
+
+        try {
+            byte[] bytes = data.getBytes("UTF-8");
+            dataStream = new ByteArrayInputStream(bytes);
+            contentLength = bytes.length;
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return this.invokeAction(url,
+                                 name,
+                                 fileName,
+                                 dataStream,
+                                 contentLength,
+                                 contentType);
+    }
+
+    public String invokeAction(String url,
+                               String name,
+                               File dataFile,
+                               String contentType)
+        throws FileNotFoundException {
+
+        InputStream dataStream = new FileInputStream(dataFile);
+        return this.invokeAction(url,
+                                 name,
+                                 dataFile.getName(),
+                                 dataStream,
+                                 (int) dataFile.length(),
+                                 contentType);
+    }
+
+    public String invokeAction(String url,
+                               String name,
+                               String fileName,
+                               InputStream dataStream,
+                               int contentLength,
+                               String contentType) {
 
         // Send a request to iTunes U and record the response.
         StringBuffer response = null;
@@ -59,13 +103,17 @@ class ITunesUFilePOST {
                                                 + url + "\" does not use HTTPS.");
             }
 
-            // Build the multipart data.
-            MimeMultipart multipart = new MimeMultipart();
-            MultipartFormBuilder.addMultipartFormBody(multipart,
-                                                      name,
-                                                      fileName,
-                                                      data,
-                                                      contentType);
+            String boundary = createBoundary();
+            
+            byte[] header = ("--" + boundary + "\r\n"
+                             + "Content-Disposition: form-data; name=\"" + name
+                             + "\"; filename=\"" + fileName + "\"\r\n"
+                             + "Content-Type: " + contentType + "\r\n"
+                             + "\r\n").getBytes("UTF-8");
+            contentLength += header.length;
+
+            byte[] footer = ("\r\n--" + boundary + "--\r\n").getBytes("UTF-8");
+            contentLength += footer.length;
 
             // Create a connection to the requested iTunes U URL.
             HttpURLConnection connection =
@@ -74,15 +122,20 @@ class ITunesUFilePOST {
             connection.setDoOutput(true);
             connection.setRequestMethod("POST");
             connection.setRequestProperty("Content-Type",
-                                          "multipart/form-data; boundary="
-                                          + MultipartFormBuilder.getMultipartBoundary(multipart));
+                                          "multipart/form-data; boundary=\""
+                                          + boundary + "\"");
+            connection.setFixedLengthStreamingMode(contentLength);
 
-            String multipartData = MultipartFormBuilder.getMultipartData(multipart);
-            
             // Send the multipart data to iTunes U.
             connection.connect();
             OutputStream output = connection.getOutputStream();
-            output.write(multipartData.getBytes("UTF-8"));
+            output.write(header);
+            byte[] dataBuffer = new byte[16 * 1024];
+            for (int n = 0; n >= 0;) {
+                n = dataStream.read(dataBuffer, 0, dataBuffer.length);
+                if (n > 0) output.write(dataBuffer, 0, n);
+            }
+            output.write(footer);
             output.flush();
             output.close();
 
@@ -112,5 +165,37 @@ class ITunesUFilePOST {
 
         // Return the response received from iTunes U.
         return response.toString();
+    }
+
+    private static String createBoundary() {
+        return createBoundary(0);
+    }
+
+    private static String createBoundary(int number) {
+        MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        digest.update(String.valueOf(Math.random()).getBytes());
+        digest.update(String.valueOf(System.currentTimeMillis()).getBytes());
+        digest.update(String.valueOf(digest.hashCode()).getBytes());
+        byte[] bytes = digest.digest();
+
+        String paddedNumber = Integer.toString(number);
+        paddedNumber = ("0000000000".substring(0, 10 - paddedNumber.length())
+                        + paddedNumber);
+
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("---------------------------------=__");
+        for (int i = 0; i < 8; i++) {
+            String hex =
+                Integer.toHexString((bytes[i] & 0xff) + 0x100).substring(1);
+            buffer.append(hex);
+        }
+        buffer.append('_');
+        buffer.append(paddedNumber);
+        return buffer.toString();
     }
 }
