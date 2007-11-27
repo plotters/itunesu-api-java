82a982b815084916eebd57c3cbb80d25851b42e6
dave.benjamin <dave.benjamin@7fe4d8d9-7835-0410-8305-53dfd29555a2>
2007-11-27 20:54:50 +0000

    Restored constructors to previous behavior, moved mandatory fields to sample*() methods in XsdTest, verified parameter order against schema

--- src/test/edu/asu/itunesu/XsdTest.java
+++ src/test/edu/asu/itunesu/XsdTest.java
@@ -20,6 +20,7 @@ import edu.asu.itunesu.ITunesUDocument;
 import edu.asu.itunesu.Permission;
 import edu.asu.itunesu.Section;
 import edu.asu.itunesu.Site;
+import edu.asu.itunesu.Templates;
 import edu.asu.itunesu.Track;
 
 public class XsdTest extends TestCase {
@@ -51,36 +52,36 @@ public class XsdTest extends TestCase {
     }
 
     public void testMergeSite() throws Exception {
-        Site site = new Site();
+        Site site = sampleSite();
         ITunesUDocument doc = ITunesUDocument.buildMergeSite("123456", site, false, true);
         this.requestValidator.validate(buildSource(doc.toXml()));
     }
 
     public void testMergeSiteNoHandle() throws Exception {
-        Site site = new Site();
+        Site site = sampleSite();
         ITunesUDocument doc = ITunesUDocument.buildMergeSite(null, site, false, true);
         this.requestValidator.validate(buildSource(doc.toXml()));
     }
 
     public void testAddDivision() throws Exception {
-        Division division = new Division();
+        Division division = sampleDivision();
         ITunesUDocument doc = ITunesUDocument.buildAddDivision("123456", division);
         this.requestValidator.validate(buildSource(doc.toXml()));
     }
 
-    public void testDeleteDivision() throws Exception {
+	public void testDeleteDivision() throws Exception {
         ITunesUDocument doc = ITunesUDocument.buildDeleteDivision("123456");
         this.requestValidator.validate(buildSource(doc.toXml()));
     }
 
     public void testMergeDivision() throws Exception {
-        Division division = new Division();
+        Division division = sampleDivision();
         ITunesUDocument doc = ITunesUDocument.buildMergeDivision("123456", division, true, false);
         this.requestValidator.validate(buildSource(doc.toXml()));
     }
 
     public void testAddSection() throws Exception {
-        Section section = new Section();
+        Section section = sampleSection();
         ITunesUDocument doc = ITunesUDocument.buildAddSection("123456", section);
         this.requestValidator.validate(buildSource(doc.toXml()));
     }
@@ -91,20 +92,19 @@ public class XsdTest extends TestCase {
     }
 
     public void testMergeSection() throws Exception {
-        Section section = new Section();
+        Section section = sampleSection();
         ITunesUDocument doc = ITunesUDocument.buildMergeSection("123456", section, false, false);
         this.requestValidator.validate(buildSource(doc.toXml()));
     }
 
     public void testAddCourse() throws Exception {
-        Course course = new Course();
+        Course course = sampleCourse();
         ITunesUDocument doc = ITunesUDocument.buildAddCourse("123456", "234567", course);
-        System.out.println(doc.toXml());
         this.requestValidator.validate(buildSource(doc.toXml()));
     }
 
     public void testAddCourseNoTemplateHandle() throws Exception {
-        Course course = new Course();
+        Course course = sampleCourse();
         ITunesUDocument doc = ITunesUDocument.buildAddCourse("123456", null, course);
         this.requestValidator.validate(buildSource(doc.toXml()));
     }
@@ -115,13 +115,13 @@ public class XsdTest extends TestCase {
     }
 
     public void testMergeCourse() throws Exception {
-        Course course = new Course();
+        Course course = sampleCourse();
         ITunesUDocument doc = ITunesUDocument.buildMergeCourse("123456", course, true, true);
         this.requestValidator.validate(buildSource(doc.toXml()));
     }
 
     public void testAddGroup() throws Exception {
-        Group group = new Group();
+        Group group = sampleGroup();
         ITunesUDocument doc = ITunesUDocument.buildAddGroup("123456", group);
         this.requestValidator.validate(buildSource(doc.toXml()));
     }
@@ -132,13 +132,13 @@ public class XsdTest extends TestCase {
     }
 
     public void testMergeGroup() throws Exception {
-        Group group = new Group();
+        Group group = sampleGroup();
         ITunesUDocument doc = ITunesUDocument.buildMergeGroup("123456", group, true, false);
         this.requestValidator.validate(buildSource(doc.toXml()));
     }
 
     public void testAddTrack() throws Exception {
-        Track track = new Track();
+        Track track = sampleTrack();
         ITunesUDocument doc = ITunesUDocument.buildAddTrack("123456", track);
         this.requestValidator.validate(buildSource(doc.toXml()));
     }
@@ -149,7 +149,7 @@ public class XsdTest extends TestCase {
     }
 
     public void testMergeTrack() throws Exception {
-        Track track = new Track();
+        Track track = sampleTrack();
         ITunesUDocument doc = ITunesUDocument.buildMergeTrack("123456", track);
         this.requestValidator.validate(buildSource(doc.toXml()));
     }
@@ -179,5 +179,81 @@ public class XsdTest extends TestCase {
     public void testDeleteCredential() throws Exception {
         ITunesUDocument doc = ITunesUDocument.buildDeleteCredential("credential");
         this.requestValidator.validate(buildSource(doc.toXml()));
+    }
+
+    private static Course sampleCourse() {
+    	Course course = new Course();
+    	course.setName("");
+    	// course.setHandle(""); - NOT AVAILABLE IN XSD
+    	course.setShortName("");
+    	course.setIdentifier("");
+    	course.setInstructor("");
+    	course.setDescription("");
+    	course.getGroups().add(sampleGroup());
+    	// course.setAllowSubscription(true); - NOT IMPLEMENTED
+    	// course.setThemeHandle(""); - NOT IMPLEMENTED
+    	return course;
+    }
+    
+    private static Division sampleDivision() {
+    	Division division = new Division();
+    	division.setName("");
+    	division.setShortName("");
+    	division.setIdentifier("");
+    	division.setAllowSubscription(false);
+    	division.getPermissions().add(new Permission("", ""));
+    	division.getSections().add(sampleSection());
+    	// division.setThemeHandle(""); - NOT IMPLEMENTED
+		return division;
+	}
+    
+    private static Group sampleGroup() {
+    	Group group = new Group();
+    	group.setName("");
+    	group.setHandle("");
+    	group.getTracks().add(sampleTrack());
+    	group.getPermissions().add(new Permission("", ""));
+    	return group;
+    }
+    
+    private static Section sampleSection() {
+    	Section section = new Section();
+    	section.setName("");
+    	section.setHandle("");
+    	section.getSectionItems().add(sampleCourse());
+    	return section;
+    }
+    
+    private static Site sampleSite() {
+    	Site site = new Site();
+    	site.setName("");
+    	site.setHandle("");
+    	site.setAllowSubscription(true);
+    	site.getPermissions().add(new Permission("", ""));
+    	site.getSections().add(sampleSection());
+    	site.setTemplates(sampleTemplates());
+    	// site.setThemeHandle(""); - NOT IMPLEMENTED
+    	return site;
+    }
+    
+    private static Templates sampleTemplates() {
+    	Templates templates = new Templates();
+    	templates.setName("");
+    	templates.setHandle("");
+    	templates.getSectionItems().add(sampleCourse());
+    	return templates;
+    }
+    
+    private static Track sampleTrack() {
+    	Track track = new Track();
+        track.setName("");
+        track.setHandle("");
+        track.setKind("");
+        track.setDiscNumber(1);
+        track.setDurationMilliseconds(0L);
+        track.setAlbumName("");
+        track.setArtistName("");
+        track.setDownloadUrl("");
+    	return track;
     }
 }
